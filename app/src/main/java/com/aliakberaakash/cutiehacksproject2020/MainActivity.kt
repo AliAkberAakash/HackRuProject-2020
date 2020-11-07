fc3c70f66ae216b507ce90e3861c9e546b5993d6
aliakberaakash <ali852609@gmail.com>
2020-11-07 23:30:05 +0600

    fixed navhost fragment

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
@@ -3,9 +3,12 @@ package com.aliakberaakash.cutiehacksproject2020
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.aliakberaakash.cutiehacksproject2020.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,10 +29,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpNavigation(){
-        navHostFragment = (supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?)!!
-        NavigationUI.setupWithNavController(binding.bottomNav,
-            navHostFragment.navController
-        )
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(this,navController)
+        NavigationUI.setupWithNavController(bottom_nav, navController)
     }
 }
