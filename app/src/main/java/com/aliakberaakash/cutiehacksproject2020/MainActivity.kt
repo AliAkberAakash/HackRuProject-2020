1e0652a9f20b89ca8e7650b8c23274238a5d684a
aliakberaakash <ali852609@gmail.com>
2020-11-07 23:14:13 +0600

    Added setup bottom nav abr

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.aliakberaakash.cutiehacksproject2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.aliakberaakash.cutiehacksproject2020.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navHostFragment : NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setUpDataBinding()
+        setUpNavigation()
+    }
+
+    private fun setUpDataBinding(){
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+    }
+
+    private fun setUpNavigation(){
+        navHostFragment = (supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?)!!
+        NavigationUI.setupWithNavController(binding.bottomNav,
+            navHostFragment.navController
+        )
     }
 }
