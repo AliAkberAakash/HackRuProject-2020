c2155908c1a61b5de254d2cb4eb36864ec459916
aliakberaakash <ali852609@gmail.com>
2020-11-07 22:51:41 +0600

    Added bottom nav

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/MainActivity.kt
@@ -2,10 +2,12 @@ package com.aliakberaakash.cutiehacksproject2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
