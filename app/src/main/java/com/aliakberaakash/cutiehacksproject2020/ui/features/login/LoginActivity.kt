cfa1e6e41cfc77ef679643c7729addd25c436eb5
aliakberaakash <ali852609@gmail.com>
2020-11-08 02:46:33 +0600

    added firebase login

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/login/LoginActivity.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.aliakberaakash.cutiehacksproject2020.ui.features.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,57 +10,104 @@ import com.aliakberaakash.cutiehacksproject2020.MainActivity
 import com.aliakberaakash.cutiehacksproject2020.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity: AppCompatActivity() {
-    val RC_SIGN_IN = 9001
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        auth = Firebase.auth
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.server_client_id))
             .requestEmail()
             .build()
 
-        var googleSignInClient = GoogleSignIn.getClient(this, gso)
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-        val startIntent = Intent(this, MainActivity::class.java).apply{
-            //passes in the user's name for display
-            if (account != null) {
-                putExtra("Name",account.displayName)
-            }
-        }
-        if(account!=null){
-            startActivity(startIntent)
-        }
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        var image: ImageView = findViewById(R.id.imageView)
+
+        val image: ImageView = findViewById(R.id.imageView)
         image.setImageResource(R.drawable.cutie)
-        val signInButton:SignInButton = findViewById (R.id.sign_in_button);
-        signInButton.setOnClickListener(View.OnClickListener {
-            val signInIntent: Intent = googleSignInClient.getSignInIntent()
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        })
 
+        sign_in_button.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        sendToMainActivity(currentUser)
+    }
+
+        private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task =
-                GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
         }
+    }
+
+    private fun sendToMainActivity(currentUser : FirebaseUser?) {
+        val startIntent = Intent(this, MainActivity::class.java).apply{
+            //passes in the user's name for display
+            if (currentUser != null) {
+                putExtra("Name",currentUser.displayName)
+
+            }
+        }
+        if(currentUser!=null)
+            startActivity(startIntent)
+
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    sendToMainActivity(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -76,6 +124,9 @@ class LoginActivity: AppCompatActivity() {
         }
     }
 
-
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
 
 }
