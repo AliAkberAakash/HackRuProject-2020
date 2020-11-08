f630fa5f3fbb16e2a2b6f5c50834a0d68ae00f7e
aliakberaakash <ali852609@gmail.com>
2020-11-08 08:30:38 +0600

    Added image upload option

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/uploadImage/UploadImageFragment.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/uploadImage/UploadImageFragment.kt
@@ -9,14 +9,17 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.aliakberaakash.cutiehacksproject2020.R
-import com.google.android.material.button.MaterialButton
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.uploadimage_layout.*
+import java.io.ByteArrayOutputStream
 import java.io.FileNotFoundException
 import java.io.IOException
+import java.sql.Timestamp
+import java.util.*
 
 
 class UploadImageFragment : Fragment() {
@@ -24,6 +27,7 @@ class UploadImageFragment : Fragment() {
         const val GET_FROM_GALLERY = 3
     }
     lateinit var bitmap:Bitmap
+    lateinit var storage: FirebaseStorage
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +40,8 @@ class UploadImageFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        storage = Firebase.storage
+
 
         choose_image.setOnClickListener {
             startActivityForResult(
@@ -58,6 +64,21 @@ class UploadImageFragment : Fragment() {
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, selectedImage)
                 imageUpload.setImageBitmap(bitmap)
+                val baos = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                val data = baos.toByteArray()
+
+                var storageRef = storage.reference
+                val currentTime = Timestamp(System.currentTimeMillis())
+                val filename = "$currentTime.jpg"
+                val imagesRef = storageRef.child("images/$filename")
+                var uploadTask = imagesRef.putBytes(data)
+                uploadTask.addOnFailureListener {
+                    // Handle unsuccessful uploads
+                }.addOnSuccessListener { taskSnapshot ->
+                    // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                    
+                }
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
             } catch (e: IOException) {
