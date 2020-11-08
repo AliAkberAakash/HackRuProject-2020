3da4280cd5ab66aeb6ecc49d49115bd72f873db4
aliakberaakash <ali852609@gmail.com>
2020-11-08 08:07:49 +0600

    tweaked add page

--- app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/uploadImage/UploadImageFragment.kt
+++ app/src/main/java/com/aliakberaakash/cutiehacksproject2020/ui/features/uploadImage/UploadImageFragment.kt
@@ -14,40 +14,37 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.aliakberaakash.cutiehacksproject2020.R
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.uploadimage_layout.*
 import java.io.FileNotFoundException
 import java.io.IOException
 
 
 class UploadImageFragment : Fragment() {
     companion object{
-        val GET_FROM_GALLERY = 3
+        const val GET_FROM_GALLERY = 3
     }
     lateinit var bitmap:Bitmap
-    lateinit var img:ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.uploadimage_layout, container, false)
-        val uploadBtn: MaterialButton = view.findViewById(R.id.uploadBtn)
-        img = view.findViewById(R.id.imageUpload)
-        uploadBtn.setOnClickListener(View.OnClickListener {
-            startActivityForResult(
-                Intent(
-                    Intent.ACTION_PICK,
-                    MediaStore.Images.Media.INTERNAL_CONTENT_URI
-                ), GET_FROM_GALLERY
-            )
-
-        })
-
 
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        choose_image.setOnClickListener {
+            startActivityForResult(
+                Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                ), GET_FROM_GALLERY
+            )
+        }
 
     }
 
@@ -60,12 +57,13 @@ class UploadImageFragment : Fragment() {
             val selectedImage: Uri? = data?.data
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, selectedImage)
+                imageUpload.setImageBitmap(bitmap)
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
             } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
-        img.setImageBitmap(bitmap)
+
     }
 }
